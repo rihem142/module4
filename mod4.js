5ea7c143557ab13f2bfb9fc4d4664f79b0363840
rihem142 <92621411+rihem142@users.noreply.github.com>
2021-11-14 20:42:48 +0100

    Update mod4.js

--- mod4.js
+++ mod4.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[0]="Yosri";
+names[1]="jane";
+names[2]="omar";
+names[3]="jackson";
+names[4]="michel";
+names[5]="celine";
+names[6]="maria";
+names[7]="bruno";
+names[8]="paul";
+names[9]="jack";
 
 
 for (var i = 0; i < names.length; i++) {
